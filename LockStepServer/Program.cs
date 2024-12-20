2fd36e41d7ffaee5f9884c6deb2155a1a5211c06
dyx77421088 <2639074625@qq.com>
2024-12-20 16:18:41 +0800

    commit修改

--- LockStepServer/Program.cs
+++ LockStepServer/Program.cs
@@ -1,4 +1,5 @@
-﻿using LockStepServer.Server;
+﻿using Commit.Config;
+using LockStepServer.Server;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- LockStepServer/Server/UdpServer.cs
+++ LockStepServer/Server/UdpServer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Commit.Config;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,15 +12,14 @@ namespace LockStepServer.Server
 {
     public class UdpServer
     {
-        private static int port = 5000;
-        private static UdpClient udpServer = new UdpClient(port);
+        private static UdpClient udpServer = new UdpClient(NetConfig.UDP_PORT);
         // 用来存放客户端的连接
         private static List<IPEndPoint> clients = new List<IPEndPoint>();
         public static void Start()
         {
             Console.WriteLine("UDP 聊天服务器已启动，等待消息...");
 
-            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, port);
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, NetConfig.UDP_PORT);
 
             //const uint IOC_IN = 0x80000000;
             //int IOC_VENDOR = 0x18000000;
@@ -29,6 +29,9 @@ namespace LockStepServer.Server
             //udpServer.Client.IOControl((int)SIO_UDP_CONNRESET, new byte[] { Convert.ToByte(false) }, null);
             ReceiveMessages();
         }
+
+
+
         /// <summary>
         /// 循环接收信息
         /// </summary>
@@ -44,7 +47,7 @@ namespace LockStepServer.Server
                     byte[] receivedData = udpServer.Receive(ref clientEndPoint);
                     Console.WriteLine(Encoding.UTF8.GetString(receivedData));
                     // 处理接收到的消息
-                    HandleBroadcastMessage(receivedData, clientEndPoint);
+                    HandleLoginMessage(receivedData, clientEndPoint);
                 }
                 catch (SocketException ex)
                 {
@@ -61,22 +64,25 @@ namespace LockStepServer.Server
         /// </summary>
         /// <param name="msg">消息</param>
         /// <param name="client">发送这个消息的客户端</param>
-        private static void HandleBroadcastMessage(byte[] msg, IPEndPoint client)
+        private static void HandleBroadcastMessage()
         {
-            // 如果这个客户端没有被添加进来，那么就添加到这个集合中
-            if (!clients.Contains(client))
-            {
-                clients.Add(client);
-            }
-            // 把这个消息广播给所有的客户端（出去发送者）
-            foreach (IPEndPoint endpoint in clients)
-            {
-                
-                if (!endpoint.Equals(client))
-                {
-                    udpServer.Send(msg, msg.Length, endpoint);
-                }
-            }
+
+        }
+
+        private static void HandleLoginMessage(byte[] loginInfo, IPEndPoint client)
+        {
+            string[] strs = Encoding.UTF8.GetString(loginInfo).Split(' ');
+            string message = CheckLogin(strs[0], strs[1]) ? "登陆成功" : "用户名或密码错误";
+            byte[] msg = Encoding.UTF8.GetBytes(message);
+            udpServer.Send(msg, msg.Length, client);
+        }
+
+        private static bool CheckLogin(string userName, string password)
+        {
+            if ("张三".Equals(userName) && "123456".Equals(password)) return true;
+            else if ("里斯".Equals(userName) && "666666".Equals(password)) return true;
+            else if ("admin".Equals(userName) && "admin".Equals(password)) return true;
+            return false;
         }
     }
 }
